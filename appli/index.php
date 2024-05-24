448d4820e806b14ebac3a425cbf99b17448fd9fd
Ehonam <164899950+Ehonam@users.noreply.github.com>
2024-05-24 17:01:23 +0200

    Update index.php

--- appli/index.php
+++ appli/index.php
@@ -7,12 +7,13 @@
     <title>Ajout produit</title>
 </head>
 <body>
-    
-<nav> <!--Permettre à l'utilisateur d'aller sur la page recap.php à tout moment, à l’aide d’une barre de navigation -->
+    <nav> <!--Permettre à l'utilisateur d'aller sur la page recap.php à tout moment, à l’aide d’une barre de navigation -->
         <ul>
             <li><a href="recap.php">Voir le récapitulatif</a></li>
         </ul>
     </nav>
+    <p>Nombre total d'articles en session : <a href="recap.php?totalQuantité=<?php echo number_format($totalQuantité, 0, ",", "&nbsp;")."&nbsp;"; ?>"></p>
+    
     <div class="table">
     <form action="traitement.php" method="post"> <!-- formulaire de renseignement produits -->
         <p>
@@ -30,7 +31,9 @@
         <p>
             <label>
                 Quantité désirée :
+                <button onclick="decrementQuantity('quantity1')">-</button>
                 <input type="number" name="qtt" value="1">
+                <button onclick="incrementQuantity('quantity1')">+</button>
             </label>
         </p>
         <p>
@@ -38,6 +41,48 @@
         </p>
     </form>
 
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> <!-- Bootstrap JS -->
-    </div>
+    
+    <!--- <?php
+    $quantities = array(1); // Tableau des quantités
+
+    echo "<table>";
+    foreach ($quantities as $qtt) {
+        echo "<tr>";
+        echo "<td><a href='?qtt=" . ($qtt + 1) . "'>+</a></td>";
+        echo "<td>" . $qtt . "</td>";
+        echo "<td><a href='?qtty=" . ($qtt - 1) . "'>-</a></td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+
+    if (isset($_GET['quantity'])) {
+        $qtt = $_GET['qtt'];
+        // Vérifier si la quantité est valide et mettre à jour le tableau des quantités
+    }
+    ?>
+    --->
+    <!--- <script>
+    function incrementQuantity(quantityId) {
+        var quantityElement = document.getElementById(quantityId);
+        var currentQuantity = parseInt(quantityElement.textContent);
+        quantityElement.textContent = currentQuantity + 1;
+    }
+
+    function decrementQuantity(quantityId) {
+        var quantityElement = document.getElementById(quantityId);
+        var currentQuantity = parseInt(quantityElement.textContent);
+        if (currentQuantity > 0) {
+        quantityElement.textContent = currentQuantity - 1;
+        }
+    }
+    </script> --->
+
+
+
+</div>
 </body>
+
+</html>
