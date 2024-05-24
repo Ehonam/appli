78c47e0c833d5d61a39652068ff4e876e22e1808
Ehonam <164899950+Ehonam@users.noreply.github.com>
2024-05-24 12:01:13 +0200

    Update index.php

--- appli/index.php
+++ appli/index.php
@@ -3,11 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" /> <!-- Bootstrap CSS, ensemble de class CSS pré fait (comme ça, même pas besoin de fichier CSS) -->
     <title>Ajout produit</title>
 </head>
 <body>
-    <h1>Ajouter un produit</h1>
-    <form action="traitement.php" method="post"> /* formulaire de renseignement produits */
+    
+<nav> <!--Permettre à l'utilisateur d'aller sur la page recap.php à tout moment, à l’aide d’une barre de navigation -->
+        <ul>
+            <li><a href="recap.php">Voir le récapitulatif</a></li>
+        </ul>
+    </nav>
+    <div class="table">
+    <form action="traitement.php" method="post"> <!-- formulaire de renseignement produits -->
         <p>
             <label>
                 Nom du produit :
@@ -30,5 +37,7 @@
             <input type="submit" name="submit" value="Ajouter le produit">
         </p>
     </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> <!-- Bootstrap JS -->
+    </div>
 </body>
-</html>
