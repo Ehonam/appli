1ed04931f2f1bc3d037e18992dbd2151a1fe344f
Ehonam <164899950+Ehonam@users.noreply.github.com>
2024-05-27 17:11:01 +0200

    Update index.php

--- appli/index.php
+++ appli/index.php
@@ -1,88 +1,92 @@
+<?php
+session_start();
+// var_dump($_SESSION["message"]);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="description" content="Site de vente de produit">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" /> <!-- Bootstrap CSS, ensemble de class CSS pré fait (comme ça, même pas besoin de fichier CSS) -->
     <title>Ajout produit</title>
 </head>
 <body>
+
+    <div id="wrapper">
+
     <nav> <!--Permettre à l'utilisateur d'aller sur la page recap.php à tout moment, à l’aide d’une barre de navigation -->
         <ul>
             <li><a href="recap.php">Voir le récapitulatif</a></li>
         </ul>
     </nav>
-    <p>Nombre total d'articles en session : <a href="recap.php?totalQuantité=<?php echo number_format($totalQuantité, 0, ",", "&nbsp;")."&nbsp;"; ?>"></p>
-    
-    <div class="table">
-    <form action="traitement.php" method="post"> <!-- formulaire de renseignement produits -->
-        <p>
-            <label>
-                Nom du produit :
-                <input type="text" name="name">
-            </label>
-        </p>
-        <p>
-            <label>
-                Prix du produit :
-                <input type="number" step="any" name="price">
-            </label>
-        </p>
-        <p>
-            <label>
-                Quantité désirée :
-                <button onclick="decrementQuantity('quantity1')">-</button>
-                <input type="number" name="qtt" value="1">
-                <button onclick="incrementQuantity('quantity1')">+</button>
-            </label>
-        </p>
-        <p>
-            <input type="submit" name="submit" value="Ajouter le produit">
-        </p>
-    </form>
 
+        <main class="addProduct">
+            <h1>Ajouter un produit</h1>
 
+            <!-- Fichier à atteindre lorsque qu'on soumet le formulaire avec la méthode http -->
+            <form action="traitement.php?action=add" method="post"> <!-- formulaire de renseignement produits -->
+                <p>
+                    <label>
+                        Nom du produit :
+                        <input type="text" name="name" class="form-control" maxlength="50">
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Prix du produit :
+                        <input type="number" step="any" name="price" class="form-control">
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Quantité désirée :
+                        <input type="number" name="qtt" value="1" class="form-control" min="1">
+                    </label>
+                </p>
+                <p>
+                    <input type="submit" class="btn btn-success" name="submit" value="Ajouter le produit">
+                </p>
+            </form>
+            <div>
+                <p>Nombre de produits présents en session :
+                <?php
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> <!-- Bootstrap JS -->
-    
-    <!--- <?php
-    $quantities = array(1); // Tableau des quantités
+                if(!isset($_SESSION['products']) || empty($_SESSION['products'])){
+                    echo "0";
 
-    echo "<table>";
-    foreach ($quantities as $qtt) {
-        echo "<tr>";
-        echo "<td><a href='?qtt=" . ($qtt + 1) . "'>+</a></td>";
-        echo "<td>" . $qtt . "</td>";
-        echo "<td><a href='?qtty=" . ($qtt - 1) . "'>-</a></td>";
-        echo "</tr>";
-    }
-    echo "</table>";
+                } else {
+                    $result = 0;
+                    foreach($_SESSION['products'] as $index => $product){
+                        $result = $result + $product['qtt'];
+                    }
+                    echo $result;
+                }
+                echo "</p>";
 
-    if (isset($_GET['quantity'])) {
-        $qtt = $_GET['qtt'];
-        // Vérifier si la quantité est valide et mettre à jour le tableau des quantités
-    }
-    ?>
-    --->
-    <!--- <script>
-    function incrementQuantity(quantityId) {
-        var quantityElement = document.getElementById(quantityId);
-        var currentQuantity = parseInt(quantityElement.textContent);
-        quantityElement.textContent = currentQuantity + 1;
-    }
+                if(!isset($_SESSION['message']) || empty($_SESSION['message'])){
+                    echo "";
+                } else {
+                    echo "<p class='success'>".$_SESSION["message"]."</p>"; 
+                    unset($_SESSION["message"]);
+                }
+                
+                if(!isset($_SESSION['error']) || empty($_SESSION['error'])){
+                    echo "";
+                } else {
+                    echo "<p class='error'>".$_SESSION["error"]."</p>";  
+                    unset($_SESSION["error"]);
+                    
+                }
+                ?>
+            </div>
+        </main>
 
-    function decrementQuantity(quantityId) {
-        var quantityElement = document.getElementById(quantityId);
-        var currentQuantity = parseInt(quantityElement.textContent);
-        if (currentQuantity > 0) {
-        quantityElement.textContent = currentQuantity - 1;
-        }
-    }
-    </script> --->
+    </div>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
-
-</div>
 </body>
-
 </html>
