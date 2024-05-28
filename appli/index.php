e0df1888224125330e5f7e556392b87ed35d73d0
Ehonam <164899950+Ehonam@users.noreply.github.com>
2024-05-28 11:48:14 +0200

    Update index.php

--- appli/index.php
+++ appli/index.php
@@ -10,20 +10,21 @@ session_start();
     <meta name="description" content="Site de vente de produit">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ajout produit</title>
 </head>
 <body>
 
-    <div id="wrapper">
+    <div id="wrapper" style=color:green;">
 
     <nav> <!--Permettre à l'utilisateur d'aller sur la page recap.php à tout moment, à l’aide d’une barre de navigation -->
         <ul>
-            <li><a href="recap.php">Voir le récapitulatif</a></li>
+            <li><a href="recap.php" style="color:white;">Voir le récapitulatif</a></li>
         </ul>
     </nav>
 
-        <main class="addProduct">
+        <main class="addProduct" style="background-color:white;">
             <h1>Ajouter un produit</h1>
 
             <!-- Fichier à atteindre lorsque qu'on soumet le formulaire avec la méthode http -->
